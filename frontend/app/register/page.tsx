974246f332b2667ab2e2a196444e693f86c742a6
takasi <takasi_0118@icloud.com>
2026-02-05 20:52:03 +0900

    loginback機能追加

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -2,14 +2,16 @@
 
 import Input from "@/components/ui/Input";
 import { useRegister } from "@/features/register/hooks/useRegister";
+import { useRouter } from "next/navigation";
 
 export default function SignPage() {
+  const router = useRouter();
   const { name, email, password, setName, setEmail, setPassword, handleSign } = useRegister();
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="w-full max-w-sm bg-white p-6 rounded shadow">
-        <h1 className="text-[25px] leading-[1.5] font-bold mb-5">新規登録</h1>
+        <h1 className="text-2xl font-bold mb-5">新規登録</h1>
 
         <Input
           type="text"
@@ -34,10 +36,21 @@ export default function SignPage() {
 
         <button
           onClick={handleSign}
-          className="w-full bg-blue-500 text-white p-2 rounded"
+          className="w-full bg-blue-500 text-white p-2 rounded mb-3"
         >
           新規登録
         </button>
+
+        <div className="text-center text-sm">
+          <span
+            className="text-blue-600 cursor-pointer hover:underline"
+            onClick={() => router.push("/login")}
+          >
+            ログインへ戻る
+          </span>
+        </div>
+
+
       </div>
     </div>
   );
